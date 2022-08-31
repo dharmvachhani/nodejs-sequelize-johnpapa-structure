ad1fe81c462aec2a8386e469aaa30ccc671741d5
Dharm Vachhani <dharmvachhani256@gmail.com>
2022-09-01 00:07:17 +0530

    initial

---
+++ modules/user/model.js
@@ -0,0 +1,18 @@
+"use strict";
+const Sequelize = require("sequelize");
+const sequelize = require("../../config/db");
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
